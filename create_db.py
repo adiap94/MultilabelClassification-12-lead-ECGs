b6e95646157c81ff14052c5940e529f1ca5d139c
adi.a <adi.a@solocate.com>
2022-04-28 19:45:22 +0300

    update create_db

--- create_db.py
+++ create_db.py
@@ -15,7 +15,7 @@ def get_test_code(dx_mapping_scored_path):
     test_code_list = [str(x) for x in test_code_list]
 
     return test_code_list
-def main(source_dir):
+def main(source_dir,out_dir,val_ratio=0.15, test_ratio=0.15):
     mat_files = [os.path.join(root, f) for root, subdirs, files in os.walk(source_dir) for f in files if
                f.lower().endswith(".mat")]
     #create dataframe
@@ -29,23 +29,21 @@ def main(source_dir):
         for dx_code in Dx_code_list:
             if dx_code in test_code_list:
                 df.loc[index,"potential_code"]=1
-    # Extract type
-    db["type"] = db.seg.apply(lambda x: x.split(os.sep)[-3])
 
-    # train
-    idx = db.groupby(["type"]).sample(frac=0.8).index
-    db.loc[idx,"set"] = "train"
+    idx = df[df.potential_code==1].sample(frac=test_ratio).index
+    df.loc[idx,"set"] = "test"
 
-    # val
-    idx = db[db.set.isna()].groupby(["type"]).sample(frac=0.5).index
-    db.loc[idx,"set"] = "val"
+    idx = df[df.set.isna()].groupby(["potential_code"]).sample(frac=val_ratio).index
+    df.loc[idx,"set"] = "val"
 
-    # test
-    db.set.fillna(value="test" , inplace=True)
+    df.set.fillna(value="train" , inplace=True)
+
+    df.to_csv(os.path.join(out_dir,"db.csv"),index=False)
 
     print("finish creating db")
+    print("sava db to: " + os.path.join(out_dir,"db.csv"))
 if __name__ == '__main__':
     dx_mapping_scored_path ="/tcmldrive/project_dl/code/adi/utils/dx_mapping_scored.csv"
-
+    out_dir = "/tcmldrive/project_dl/db/"
     source_dir="/tcmldrive/project_dl/data/"
-    main(source_dir=source_dir)
+    main(source_dir=source_dir,out_dir=out_dir)
