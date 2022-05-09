ecd8a19ceb19f063bb8fdb744679ee0ed7de9984
adi.a <adi.a@solocate.com>
2022-05-09 21:28:15 +0300

    organize code

--- train_model.py
+++ train_model.py
@@ -3,10 +3,13 @@
 import os, sys
 from train_12ECG_classifier import train_12ECG_classifier
 
-if __name__ == '__main__':
+def main():
 
     print('Running training code...')
 
     train_12ECG_classifier()
 
     print('Done.')
+if __name__ == '__main__':
+    main()
+    pass
