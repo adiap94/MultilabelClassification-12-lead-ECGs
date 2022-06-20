b813dad76d2feac88c4d97ca78c3e9819347933d
adi.a <adi.a@solocate.com>
2022-06-20 18:16:56 +0300

    update calc_rri

--- utils/calc_rri.py
+++ utils/calc_rri.py
@@ -22,15 +22,15 @@ def get_rri(sig, fs=300, n_beats=10):
     for i in range(sig_n.shape[0]):
         r_peaks = detectors.pan_tompkins_detector(sig_n[i])
         rri = np.true_divide(np.diff(r_peaks), fs)
-        if len(rri) < n_beats:
-            rri = np.pad(rri, (0, n_beats - len(rri)), 'constant', constant_values=(0))
-        if len(rri) > n_beats:
-            rri = rri[0:n_beats]
-        rri_list.append(rri)
-
-    rri_stack = np.stack(rri_list, axis=0)
+        # if len(rri) < n_beats:
+        #     rri = np.pad(rri, (0, n_beats - len(rri)), 'constant', constant_values=(0))
+        # if len(rri) > n_beats:
+        #     rri = rri[0:n_beats]
+        # rri_list.append(rri)
+        rri_list.append(np.mean(rri))
+    # rri_stack = np.stack(rri_list, axis=0)
     # print(rri_stack.shape)
-    return rri_stack
+    return np.array(rri_list)
 
 
 if __name__ == '__main__':
